866761e95a3e30ff0997df2b0cfb7f5bc3d028d6
Pedro Ramaciotti <pedroramaciotti@users.noreply.github.com>
2022-03-04 15:55:22 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+
+from distutils.core import setup
+
+setup(
+  name = 'CORG',         
+  version = '0.1',      
+  license='MIT',        
+  description = 'CORG: Corporal geometrics for text dimensionality analysis',   
+  author = 'Pedro Ramaciotti Morales',                      
+  url = 'https://github.com/pedroramaciotti/CORG',   
+  keywords = ['NLP'],
+  install_requires=[            
+          'numpy',
+          'pandas',
+          'scipy',
+      ],
+  packages = ["corg"],
+  include_package_data=True,
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
