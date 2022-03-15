de4d90f01e02eaf6aa6bcfb8fd411fbd078d6220
Foula <foula.vagena@gmail.com>
2022-03-15 14:07:16 +0100

    F1

--- corg/__init__.py
+++ corg/__init__.py
@@ -1,1 +1,5 @@
-print('hello world')
+from .single_ca_dimension_benchmark import SingleCADimensionBenchmark
+
+from .__version__ import __version__
+
+__all__ = ['__version__', 'SingleCADimensionBenchmark']
