f94cd782c82736537d8de1d0bc68bb9363efc44f
Pedro Ramaciotti <pedroramaciotti@users.noreply.github.com>
2022-05-19 09:22:01 +0200

    release 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
   name = 'CORG',         
-  version = '0.1',      
+  version = '0.2',      
   license='MIT',        
   description = 'CORG: Corporal geometrics for text dimensionality analysis',   
   author = 'Pedro Ramaciotti Morales',                      
